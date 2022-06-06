e3f338cd97ab977eee2a1ae26bb009678cc16504
jason606 <stonegee606@yahoo.com>
2022-06-06 11:56:16 +0000

    Desktop

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const navIcon = document.querySelector(".nav__menu-icon");
 const cartIcon = document.querySelector(".cart-icon");
 navIcon.addEventListener("click", () => {
-  console.log("clicked");
   const navList = document.querySelector(".nav__list-menu");
   const closeMenu = document.querySelector(".nav-close");
   navList.style.display = "block";
@@ -13,8 +12,32 @@ navIcon.addEventListener("click", () => {
 cartIcon.addEventListener("click", () => {
   console.log("clicked");
   const shoppingCart = document.querySelector(".shopping-cart");
-  shoppingCart.classList.toggle("active");
+  shoppingCart.classList.toggle("cart-active");
 });
 
-document.querySelector("");
 document.querySelector(".cart-items");
+
+let thumbnail = document.querySelectorAll(".thumbnail");
+
+for (let i = 0; i < thumbnail.length; i++) {
+  thumbnail[i].addEventListener("click", () => {
+    
+let activeImages = document.querySelectorAll(".active")
+
+    if(activeImages.length > 0){
+      activeImages[0].classList.remove('active')
+    }
+
+    thumbnail[i].classList.add("active")
+    document.querySelector(".product-image").src = thumbnail[i].src.replace('-thumbnail', '')
+    
+  });
+}
+    let leftBtn =  document.querySelector(".nav__btn-left")
+    leftBtn.addEventListener("click", ()=>{
+      console.log('click')
+    })
+    let rightBtn = document.querySelector(".nav__btn-right")
+    rightBtn.addEventListener("click", ()=>{
+      console.log("click")
+    })
